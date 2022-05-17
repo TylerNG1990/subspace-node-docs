d198333c78566770992c0cf2bb14fa6bcb78c53f
syskey <lopetvers@gmail.com>
2022-05-17 09:31:56 +0800

    update snapshot-2022-may-03

--- run.sh
+++ run.sh
@@ -3,12 +3,18 @@
 # @Date:   2022/4/12
 # @twitter:https://twitter.com/skyMetaverse
 
-wget https://github.com/subspace/subspace/releases/download/snapshot-2022-mar-09/subspace-node-ubuntu-x86_64-snapshot-2022-mar-09
-mv subspace-node-ubuntu-x86_64-snapshot-2022-mar-09 node
-wget https://github.com/subspace/subspace/releases/download/snapshot-2022-mar-09/subspace-farmer-ubuntu-x86_64-snapshot-2022-mar-09
-mv subspace-farmer-ubuntu-x86_64-snapshot-2022-mar-09 farmer
+# wget https://github.com/subspace/subspace/releases/download/snapshot-2022-mar-09/subspace-node-ubuntu-x86_64-snapshot-2022-mar-09
+
+wget https://github.com/subspace/subspace/releases/download/snapshot-2022-may-03/subspace-node-ubuntu-x86_64-snapshot-2022-may-03
+mv subspace-node-ubuntu-x86_64-snapshot-2022-may-03 node
+
+# wget https://github.com/subspace/subspace/releases/download/snapshot-2022-mar-09/subspace-farmer-ubuntu-x86_64-snapshot-2022-mar-09
+
+wget https://github.com/subspace/subspace/releases/download/snapshot-2022-may-03/subspace-farmer-ubuntu-x86_64-snapshot-2022-may-03
+mv subspace-farmer-ubuntu-x86_64-snapshot-2022-may-03 farmer
 chmod +x node.sh farmer.sh node farmer
 ./node.sh
+sleep 10
 ./farmer.sh
 
 echo "运行成功"
