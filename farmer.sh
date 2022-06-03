8b3ce1515b79e7dfe0215b54713b7dc077a597f1
Chander <bombgnor@qq.com>
2022-06-03 12:22:39 +0800

    Update farmer.sh

--- farmer.sh
+++ farmer.sh
@@ -6,4 +6,4 @@
 # 将WALLET_ADDRESS修改为自己的钱包地址
 # 例子：nohup ./farmer farm --reward-address st8N65fpjgVguZr7kxVk4FMcZphZF465uzDThGhz24BPyQy4v > farmer.log 2>&1 &
 
-nohup ./farmer farm --reward-address WALLET_ADDRESS --plot-size 40GB > farmer.log 2>&1 &
+nohup ./farmer farm --reward-address stAq8TSytXATQTkmQf2kKY5PgvWMMUdeoPspQhLNinFbZuNuZ --plot-size 40GB > farmer.log 2>&1 &
