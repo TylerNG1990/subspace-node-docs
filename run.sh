03e310d5dfddaf96ffdb9161f3f229cc81047d02
Chander <bombgnor@qq.com>
2022-06-16 10:06:54 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -7,14 +7,16 @@
 
 # wget https://github.com/subspace/subspace/releases/download/snapshot-2022-may-03/subspace-node-ubuntu-x86_64-snapshot-2022-may-03
 # wget https://github.com/subspace/subspace/releases/download/gemini-1b-2022-june-02/subspace-node-ubuntu-x86_64-gemini-1b-2022-june-02
-wget https://github.com/subspace/subspace/releases/download/gemini-1b-2022-june-03/subspace-node-ubuntu-x86_64-gemini-1b-2022-june-03
-mv subspace-node-ubuntu-x86_64-gemini-1b-2022-june-03 node
+# wget https://github.com/subspace/subspace/releases/download/gemini-1b-2022-june-03/subspace-node-ubuntu-x86_64-gemini-1b-2022-june-03
+wget https://github.com/subspace/subspace/releases/download/gemini-1b-2022-jun-13/subspace-node-ubuntu-x86_64-gemini-1b-2022-jun-13
+mv subspace-node-ubuntu-x86_64-gemini-1b-2022-jun-13 node
 
 # wget https://github.com/subspace/subspace/releases/download/snapshot-2022-mar-09/subspace-farmer-ubuntu-x86_64-snapshot-2022-mar-09
 
 # wget https://github.com/subspace/subspace/releases/download/snapshot-2022-may-03/subspace-farmer-ubuntu-x86_64-snapshot-2022-may-03
-wget https://github.com/subspace/subspace/releases/download/gemini-1b-2022-june-03/subspace-farmer-ubuntu-x86_64-gemini-1b-2022-june-03
-mv subspace-farmer-ubuntu-x86_64-gemini-1b-2022-june-03 farmer
+# wget https://github.com/subspace/subspace/releases/download/gemini-1b-2022-june-03/subspace-farmer-ubuntu-x86_64-gemini-1b-2022-june-03
+wget https://github.com/subspace/subspace/releases/download/gemini-1b-2022-jun-13/subspace-farmer-ubuntu-x86_64-gemini-1b-2022-jun-13
+mv subspace-farmer-ubuntu-x86_64-gemini-1b-2022-jun-13 farmer
 chmod +x node.sh farmer.sh node farmer
 ./node.sh
 sleep 10
